362f958d6ff5b0cc73350f2a18f5cb64b9b5a965
TheCsWorld <43708569+TheCsWorld@users.noreply.github.com>
2019-02-20 14:11:26 +0000

    Add files via upload

--- SortComparison/src/SortComparison.java
+++ SortComparison/src/SortComparison.java
@@ -502,11 +502,11 @@ import java.util.Arrays;
 			fr = new FileReader("C:\\Users\\chloe\\Documents\\numbersReverse1000.txt");
 			double[] arr1000r = readFile(fr);
 
-			//insertion
-			type = "insertion";		
-			time = getTime(arr1000r, type);
-			System.out.println("numbersReverse1000 insertion Time:" + time);
-			
+//			//insertion
+//			type = "insertion";		
+//			time = getTime(arr1000r, type);
+//			System.out.println("numbersReverse1000 insertion Time:" + time);
+//			
 //			//quick
 //			type = "quick";
 //			time = getTime(arr1000r, type);			
@@ -528,9 +528,33 @@ import java.util.Arrays;
 //			System.out.println("numbersReverse1000 selection Time:" + time);
 			
 			//read in numbersSorted1000
-			fr = new FileReader("C:\\Users\\chloe\\Documents\\numbersReverse1000.txt");
+			fr = new FileReader("C:\\Users\\chloe\\Documents\\numbersSorted1000.txt");
 			double[] arr1000s = readFile(fr);
-			type = "insertion";
+
+			//insertion
+			type = "insertion";		
+			time = getTime(arr1000s, type);
+			System.out.println("numbersSorted1000 insertion Time:" + time);
+			
+			//quick
+			type = "quick";
+			time = getTime(arr1000s, type);			
+			System.out.println("numbersSorted1000 quick Time:" + time);
+			
+			//mergeIterate
+			type = "mergeIt";
+			time = getTime(arr1000s, type);			
+			System.out.println("numbersSorted1000 mergeIt Time:" + time);
+			
+			//mergeRecursive
+			type = "mergeRec";
+			time = getTime(arr1000s, type);			
+			System.out.println("numbersSorted1000 mergeRec Time:" + time);
+			
+			//selection
+			type = "selection";
+			time = getTime(arr1000s, type);			
+			System.out.println("numbersSorted1000 selection Time:" + time);
 			
 			
     	  
